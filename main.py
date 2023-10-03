9c6dd8fab9f5f2a7fb0e1b4995f23b562acbde2d
Anthony Dang <anthonydang1999@gmail.com>
2023-10-02 20:00:15 -0700

    Make background color dark blue in main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame, sys
 
 pygame.init()
+dark_blue = (44, 44, 127)
 
 screen = pygame.display.set_mode((300, 600))
 pygame.display.set_caption("Python Tetris")
@@ -13,6 +14,7 @@ while True:
             pygame.quit()
             sys.exit()
 
+    screen.fill(dark_blue)
     pygame.display.update()
     clock.tick(60)
 
