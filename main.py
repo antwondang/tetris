3494a1c498600f37b012f1b662c02183b9af6705
Anthony Dang <anthonydang1999@gmail.com>
2023-10-03 20:29:24 -0700

    Test display grid in main.py

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ pygame.display.set_caption("Python Tetris")
 clock = pygame.time.Clock()
 
 game_grid = Grid()
+
+game_grid.grid[0][0] = 1
+game_grid.grid[3][5] = 4
+game_grid.grid[17][8] = 7
+
 game_grid.print_grid()
 
 while True:
@@ -17,8 +22,10 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
+            
     screen.fill(dark_blue)
+    game_grid.draw(screen)
+
     pygame.display.update()
     clock.tick(60)
 
