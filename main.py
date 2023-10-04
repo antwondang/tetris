f6365fd3e8b6cb56076be75b1cafbfb2e83c194c
Anthony Dang <anthonydang1999@gmail.com>
2023-10-03 19:50:20 -0700

    Import class from grid.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame, sys
+from grid import Grid
 
 pygame.init()
 dark_blue = (44, 44, 127)
@@ -7,6 +8,8 @@ screen = pygame.display.set_mode((300, 600))
 pygame.display.set_caption("Python Tetris")
 
 clock = pygame.time.Clock()
+
+
 
 while True:
     for event in pygame.event.get():
