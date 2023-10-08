09210ad2ed45262bf84bdbe2c49b136a4f486a04
Anthony Dang <anthonydang1999@gmail.com>
2023-10-08 16:08:07 -0700

    Create block position in blocks.py

--- blocks.py
+++ blocks.py
@@ -4,4 +4,9 @@ from position import Position
 class LBlock(Block):
     def __init__(self):
         super().__init__(id = 1)
-        self.cells = {}
+        self.cells = {
+            0: [Position(0,2), Position(1,0), Position(1,1), Position(1,2)],
+            1: [Position(0,1), Position(1,1), Position(2,1), Position(2,2)],
+            2: [Position(1,0), Position(1,1), Position(1,2), Position(2,0)],
+            3: [Position(0,0), Position(0,1), Position(1,1), Position(2,1)],
+        }
