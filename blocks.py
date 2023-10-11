b679d8cc29f11879b05e2e77abb26a0c9c26fa97
Anthony Dang <anthonydang1999@gmail.com>
2023-10-10 19:20:28 -0700

    Create JBlock in blocks.py

--- blocks.py
+++ blocks.py
@@ -10,3 +10,13 @@ class LBlock(Block):
             2: [Position(1,0), Position(1,1), Position(1,2), Position(2,0)],
             3: [Position(0,0), Position(0,1), Position(1,1), Position(2,1)],
         }
+
+class JBlock(Block):
+    def __init__(self):
+        super().__init__(id = 1)
+        self.cells = {
+            0: [Position(0,0), Position(1,0), Position(1,1), Position(1,2)],
+            1: [Position(0,1), Position(0,2), Position(1,1), Position(2,1)],
+            2: [Position(1,0), Position(1,1), Position(1,2), Position(2,2)],
+            3: [Position(0,1), Position(1,1), Position(2,1), Position(2,1)],
+        }
