f12136def9f54ffbca1086241d9aea6f4c9d60ac
Anthony Dang <anthonydang1999@gmail.com>
2023-10-10 19:27:06 -0700

    Created Z block in blocks.py

--- blocks.py
+++ blocks.py
@@ -60,3 +60,13 @@ class tBlock(Block):
             2: [Position(1,0), Position(1,1), Position(1,2), Position(2,1)],
             3: [Position(0,1), Position(1,0), Position(1,1), Position(2,1)],
         }
+
+class zBlock(Block):
+    def __init__(self):
+        super().__init__(id = 1)
+        self.cells = {
+            0: [Position(0,0), Position(0,1), Position(1,1), Position(1,2)],
+            1: [Position(0,2), Position(1,1), Position(1,2), Position(2,1)],
+            2: [Position(1,0), Position(1,1), Position(2,1), Position(2,2)],
+            3: [Position(0,1), Position(1,0), Position(1,1), Position(2,0)],
+        }
