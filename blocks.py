a581b549b3d41992d98af14eb229126ed6b6fc84
Anthony Dang <anthonydang1999@gmail.com>
2023-10-10 19:40:19 -0700

    Fix empty space of I block in blocks.py

--- blocks.py
+++ blocks.py
@@ -32,7 +32,7 @@ class iBlock(Block):
             2: [Position(2,0), Position(2,1), Position(2,2), Position(2,3)],
             3: [Position(0,1), Position(1,1), Position(2,1), Position(3,1)],
         }
-        self.move(0,3)
+        self.move(-1,3)
 
 class oBlock(Block):
     def __init__(self):
