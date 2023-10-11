7d208b3ede6b14d67e18febfd6c668530e7a3e84
Anthony Dang <anthonydang1999@gmail.com>
2023-10-10 19:49:27 -0700

    Fix bug in game.py

--- game.py
+++ game.py
@@ -10,7 +10,7 @@ class Game:
         self.next_block = self.get_random_block
 
     def get_random_block(self):
-        if len(self.blocks == 0):
+        if len(self.blocks) == 0:
             self.blocks = [iBlock(), JBlock(), LBlock(), oBlock(), tBlock(), sBlock(), zBlock()]     
         block = random.choice(self.blocks)
         self.blocks.remove(block)
