120bb725ce96353aacacbb14b258a201ec905336
Anthony Dang <anthonydang1999@gmail.com>
2023-10-10 19:22:31 -0700

    Create I block and O block in blocks.py

--- blocks.py
+++ blocks.py
@@ -20,3 +20,23 @@ class JBlock(Block):
             2: [Position(1,0), Position(1,1), Position(1,2), Position(2,2)],
             3: [Position(0,1), Position(1,1), Position(2,1), Position(2,1)],
         }
+
+class iBlock(Block):
+    def __init__(self):
+        super().__init__(id = 1)
+        self.cells = {
+            0: [Position(1,0), Position(1,1), Position(1,2), Position(1,3)],
+            1: [Position(0,2), Position(1,2), Position(2,2), Position(3,2)],
+            2: [Position(2,0), Position(2,1), Position(2,2), Position(2,3)],
+            3: [Position(0,1), Position(1,1), Position(2,1), Position(3,1)],
+        }
+
+class oBlock(Block):
+    def __init__(self):
+        super().__init__(id = 1)
+        self.cells = {
+            0: [Position(0,2), Position(1,0), Position(1,1), Position(1,2)],
+            1: [Position(0,1), Position(1,1), Position(2,1), Position(2,2)],
+            2: [Position(1,0), Position(1,1), Position(1,2), Position(2,0)],
+            3: [Position(0,0), Position(0,1), Position(1,1), Position(2,1)],
+        }
